bae2391df4c4331fade4805e52110c1c23c47804
David De La Rosa <davidrr261096@hotmail.com>
2023-09-02 00:11:28 -0400

    first setup

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const mount = (element) => {
+  ReactDOM.render(<h1>Hello World</h1>, element);
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev_marketing");
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };

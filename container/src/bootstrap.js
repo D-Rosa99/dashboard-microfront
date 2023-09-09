2f912176af66fc3d8e2f3d3e5ec077edc9304f97
David De La Rosa <davidrr261096@hotmail.com>
2023-09-09 00:03:31 -0400

    trying another actions setting

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-console.log("Hello World!!");
+console.log("Hello World!!!");
 ReactDOM.render(<App />, document.querySelector("#root"));
